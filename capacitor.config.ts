e3becd47b47d3341aac5fcf695d7efff71e2cc99
axlhnbi <axlhnbi@gmail.com>
2026-06-29 01:28:38 +0900

    adjust build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.axlhnbi.finflow',
-  appName: 'FinFlow',
+  appName: 'RinciUang',
   webDir: 'www',
   plugins: {
     SplashScreen: {
